a6b8659f28d9767b828bfa66241db88cc1042ab9
Franz-Niclas Muschter <Franz.Muschter@gmx.de>
2011-12-13 16:55:20 +0100

    initial import

---
+++ snake.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+"""snake implementation in python using pygame
+"""
+
+import sys
+import pygame
+import time
+import random
+
+WAIT	= 0.05
+RADIUS	= 10
+RES	= [800, 480]
+WALL	= []
+BUG     = ()
+pygame.init()
+SCREEN = pygame.display.set_mode(RES)
+pygame.display.set_caption("Snake by franzl")
+
+class Mob():
+    """class for moving objects
+    """
+    def __init__(self):
+        self.headx = 100
+        self.heady = 100
+        self.length = 6
+        self.elements = [[self.headx, self.heady]]
+
+        while len(self.elements) != (self.length - 1):
+            self.elements.append([self.headx, self.heady])
+        self.speed = [RADIUS * 2, 0]
+        pygame.draw.circle(SCREEN, (255, 255, 0), (self.headx, self.heady),
+            RADIUS)
+        pygame.display.flip()
+
+    def move(self):
+        """move function
+        """
+        pygame.draw.circle(SCREEN, (0, 0, 0), (self.elements[-1][0],
+            self.elements[-1][1]), RADIUS)
+        self.elements.pop()
+        self.headx += self.speed[0]
+        self.heady += self.speed[1]
+        self.elements = [[self.headx, self.heady]] + self.elements[0:]
+        self.check_dead()
+        for element in self.elements[1:]:
+            pygame.draw.circle(SCREEN, (255, 255, 0), (element[0], element[1]),
+                RADIUS)
+        pygame.draw.circle(SCREEN, (0, 255, 0), (self.headx, self.heady),
+            RADIUS)
+        pygame.display.flip()
+        self.check_bug()
+
+    def check_dead(self):
+        """check_dead function
+        """
+        if [self.headx, self.heady] in self.elements[1:]:
+            exit_dead()
+        if [self.headx, self.heady] in WALL:
+            exit_dead()
+
+    def check_bug(self):
+        """check_bug function
+        """
+        print self.headx, self.heady, BUG
+
+        if (self.headx, self.heady) == BUG:
+            print "mjamhm: %s"		% len(self.elements)
+            self.elements.append(self.elements[-1])
+            create_bug()
+
+def draw_map():
+    """draw_map function
+    """
+    for n in xrange(20, RES[0], 20):
+        pygame.draw.circle(SCREEN, (0, 0, 255), (n, 20), 10)
+        WALL.append([n, 20])
+        pygame.draw.circle(SCREEN,(0, 0, 255),(n, RES[1] - 20), 10)
+        WALL.append([n, RES[1] - 20])
+    for n in xrange(20, RES[1], 20):
+        pygame.draw.circle(SCREEN, (0, 0, 255),(20, n), 10)
+        WALL.append([20, n])
+        pygame.draw.circle(SCREEN, (0, 0, 255), (RES[0] - 20, n), 10)
+        WALL.append([RES[0] - 20 , n])
+    pygame.display.flip()
+
+def create_bug():
+    """create_bug function
+    """
+    BUG = ()
+    while ( list(BUG) in WALL ) or ( list(BUG) in SNAKE.elements) or (not BUG):
+#    while ( list(BUG) in WALL ) or ( list(BUG) in SNAKE.elements):
+        BUG = (random.randrange(40, RES[0] - 40 , 20),
+            (random.randrange(40, RES[1] - 40 , 20)))
+    print BUG	
+
+    pygame.draw.circle(SCREEN, (255, 0, 0), BUG, RADIUS)
+    pygame.display.flip()
+
+def event_loop():
+    """main event loop
+    """
+    while True:
+        time.sleep(WAIT)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_DOWN)	and \
+                    (SNAKE.speed != [0, -2*RADIUS]):
+                    SNAKE.speed = [0, 2*RADIUS]
+                elif (event.key == pygame.K_UP) and \
+                    (SNAKE.speed != [0, 2*RADIUS]):
+                    SNAKE.speed = [0, -2*RADIUS]
+                elif (event.key == pygame.K_RIGHT) and \
+                    (SNAKE.speed != [-2* RADIUS, 0]):
+                    SNAKE.speed = [2*RADIUS, 0]
+                elif (event.key == pygame.K_LEFT) and \
+                    (SNAKE.speed != [2* RADIUS, 0]):
+                    SNAKE.speed = [-2*RADIUS, 0]
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        SNAKE.move()
+
+def exit_dead():
+    """exit_dead funtion
+    """
+    time.sleep(1)
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    draw_map()
+    SNAKE = Mob()
+    create_bug()
+    event_loop()
---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+list = [[2,0],[3,0],[4,0]]
+print list
+print
+list.pop()
+
+print [1,0]
+print list[0:len(list)]
+
+print [[1,0]] + list[0:]
